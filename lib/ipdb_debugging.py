3a7da31e391bbbc4548849f71bfbbc4add1f32e0
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-08-27 15:18:03 +0300

    dohe

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,12 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+    num = num + 2
+    ipdb.set_trace()  # This line sets a breakpoint for debugging
+    return num  # The return statement is correctly indented
+
+if __name__ == "__main__":
+    # Test the function
+    test_num = 3
+    result = plus_two(test_num)
+    print(f"The result is: {result}")
